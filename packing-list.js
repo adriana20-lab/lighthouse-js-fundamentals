b0311722b2edff731a28abe923db8a0bbeda1a55
adriana20-lab <molina.adri83@gmail.com>
2020-06-02 03:15:41 +0000

    more examples

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,25 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-console.log("Kitchen stuff to pack:");
+/* console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+} */
+packingList.push("garbage bags");
+packingList.pop();
+packingList.splice(-1, 0, "garbage bags", "garbage bins");
+console.log("Kitchen stuff to pack:");
+let i = 1;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;  
 }
 
+
+// function counter(num){
+//   while(num > 0){
+//     console.log('nuuua', num)
+//     return counter(num-1)
+//   }
+// }
+
+// counter(10)
